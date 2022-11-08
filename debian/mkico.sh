580153f8a13ae75031a41bf35207ec026dbfdb2e
Vítězslav Dvořák <info@vitexsoftware.cz>
2022-11-08 08:22:56 +0100

    Build of jazz2-native, jazz2-data-shareware and jazz2-data packages #8

--- debian/mkico.sh
+++ debian/mkico.sh
@@ -7,9 +7,9 @@ resolutions='16 32 48 64 128 256 512 1024'
 
 for resolution in $resolutions
 do
-    mkdir -p $DIR/${resolution}x${resolution}/apps/
-    inkscape -w ${resolution} -h ${resolution} debian/$SRC --export-filename=$DIR/${resolution}x${resolution}/apps/$PACKAGE.png
+    mkdir -p "$DIR/${resolution}x${resolution}/apps/"
+    inkscape -w ${resolution} -h ${resolution} "debian/$SRC" --export-filename="$DIR/${resolution}x${resolution}/apps/$PACKAGE.png"
 done
-mkdir -p $DIR/scalable/apps
-cp debian/$SRC $DIR/scalable/apps/$PACKAGE.svg 
+mkdir -p "$DIR/scalable/apps"
+cp "debian/$SRC" "$DIR/scalable/apps/$PACKAGE.svg"
 echo All done
